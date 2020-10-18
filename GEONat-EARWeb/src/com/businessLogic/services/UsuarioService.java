501f326abd793e4e452ef4988153814977f00c20
Leandro Toledo <lgtoledo@outlook.com>
2020-10-18 17:44:25 -0300

    se crea un bean de sesiòn con los servicios de la capa presentacion para entidad usuario

---
+++ GEONat-EARWeb/src/com/businessLogic/services/UsuarioService.java
@@ -0,0 +1,90 @@
+package com.businessLogic.services;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.DAO.concrete.UsuarioDAO;
+import com.DAO.interfaces.IUsuarioDAO;
+import com.entities.Usuario;
+import com.exception.ServiciosException;
+import com.presentacion.dto.UsuarioDTO;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Named(value="usuarioService")
+@SessionScoped		
+public class UsuarioService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	IUsuarioDAO usuarioDAO;
+	
+	public UsuarioDTO fromUsuario(Usuario u) {
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		usuarioDTO.setId(u.getId_Usuario().intValue());
+		usuarioDTO.setNombreUsuario(u.getNombreUsuario());
+		usuarioDTO.setNombre(u.getNombre());
+		usuarioDTO.setApellido(u.getApellido());
+		usuarioDTO.setTipoDocumento(u.getTipoDocumento());
+		usuarioDTO.setNroDocumento(u.getNroDocumento());
+		usuarioDTO.setEmail(u.getEmail());
+		usuarioDTO.setDireccion(u.getDireccion());
+		usuarioDTO.setEstadoActivo(u.getEstadoActivo());
+		usuarioDTO.setEstadoActivo(u.getEstadoActivo());
+		return usuarioDTO;
+	}
+	
+	
+	public Usuario toUsuario(UsuarioDTO u) {
+		Usuario usuario = new Usuario();
+		usuario.setId_Usuario(u.getId().intValue());
+		usuario.setNombreUsuario(u.getNombreUsuario());
+		usuario.setNombre(u.getNombre());
+		usuario.setApellido(u.getApellido());
+		usuario.setTipoDocumento(u.getTipoDocumento());
+		usuario.setNroDocumento(u.getNroDocumento());
+		usuario.setEmail(u.getEmail());
+		usuario.setDireccion(u.getDireccion());
+		usuario.setEstadoActivo(u.getEstadoActivo());
+		usuario.setEstadoActivo(u.getEstadoActivo());
+		return usuario;
+	}
+	
+	
+	// servicios para capa de Presentacion
+	
+	public List<UsuarioDTO> obtenerUsuarios() throws ServiciosException {
+		List<Usuario> usuarios = usuarioDAO.obtenerTodos();
+		List<UsuarioDTO> lista = new ArrayList<UsuarioDTO>();
+		for (Usuario u : usuarios) {
+			lista.add(fromUsuario(u));
+		}
+		return lista;
+	}
+	
+	public UsuarioDTO buscarUsuario(Integer id) throws ServiciosException {
+		Usuario u = usuarioDAO.obtenerUno(id);
+		return fromUsuario(u);
+	}
+	
+	public UsuarioDTO agregarUsuario(UsuarioDTO usuarioDTO) throws ServiciosException  {
+		Usuario u = usuarioDAO.create(toUsuario(usuarioDTO));
+		return fromUsuario(u);
+	}
+
+
+	public void actualizarUsuario(UsuarioDTO usuarioDTO) throws ServiciosException   {
+		Usuario u = usuarioDAO.update(toUsuario(usuarioDTO));
+	}
+	
+	
+	
+	
+	
+}
---
+++ GEONat-EARWeb/src/com/presentacion/dto/UsuarioDTO.java
@@ -0,0 +1,154 @@
+package com.presentacion.dto;
+
+
+import com.entities.TipoDocumento;
+import com.entities.TipoUsuario;
+
+public class UsuarioDTO {
+
+	private Integer id;
+	
+	private String nombreUsuario;
+	
+	private String nombre;
+	
+	private String apellido;
+
+	private TipoDocumento tipoDocumento;
+
+	private String nroDocumento;
+
+	private String email;
+
+	private String direccion;
+
+	private Boolean estadoActivo;
+
+	private TipoUsuario tipoUsuario;
+
+	
+	// Constructores
+	public UsuarioDTO(Integer id, String nombreUsuario, String nombre, String apellido,
+			TipoDocumento tipoDocumento, String nroDocumento, String email, String direccion, TipoUsuario tipoUsuario) {
+		super();
+		this.id = id;
+		this.nombreUsuario = nombreUsuario;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.tipoDocumento = tipoDocumento;
+		this.nroDocumento = nroDocumento;
+		this.email = email;
+		this.direccion = direccion;
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	
+	public UsuarioDTO() {
+		super();
+	}
+
+
+	// Getters y Setters
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public TipoDocumento getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+
+	public void setTipoDocumento(TipoDocumento tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+
+	public String getNroDocumento() {
+		return nroDocumento;
+	}
+
+
+	public void setNroDocumento(String nroDocumento) {
+		this.nroDocumento = nroDocumento;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public Boolean getEstadoActivo() {
+		return estadoActivo;
+	}
+
+
+	public void setEstadoActivo(Boolean estadoActivo) {
+		this.estadoActivo = estadoActivo;
+	}
+
+
+	public TipoUsuario getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+
+	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+	
+	
+	
+	
+}
